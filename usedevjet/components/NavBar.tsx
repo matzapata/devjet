5dab1dd29aa121e736eda284773e072239702d9d
Matias Zapata <matuzapata@gmail.com>
2022-08-28 02:07:47 -0300

    NavBar hide buttons on mobile version

--- usedevjet/components/NavBar.tsx
+++ usedevjet/components/NavBar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrandLogo } from "./Brand";
-import { Button, Flex, HStack } from "@chakra-ui/react";
+import { Button, Flex, Hide, HStack } from "@chakra-ui/react";
 
 function NavBar() {
   return (
     <Flex my="8" justifyContent="space-between">
       <BrandLogo />
       <HStack spacing="4">
-        <Button variant="ghost">Explore</Button>
-        <Button variant="ghost">Log in</Button>
+        <Hide below="md">
+          <Button variant="ghost">Explore</Button>
+          <Button variant="ghost">Log in</Button>
+        </Hide>
         <Button colorScheme="blue">Get started</Button>
       </HStack>
     </Flex>
