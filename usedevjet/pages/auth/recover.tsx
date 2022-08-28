f63e6fb76c6ce1b91a4682ee458fa4b8ffdc032c
Matias Zapata <matuzapata@gmail.com>
2022-08-28 18:13:13 -0300

    Password recover page UI

---
+++ usedevjet/pages/auth/recover.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  Heading,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { BrandFavicon } from "../../components/Brand";
+import NextLink from "next/link";
+
+function Recover() {
+  const [state, setState] = useState({
+    value: "",
+    error: "",
+  });
+
+  const onChange = (e: any) => {
+    setState({
+      ...state,
+      value: e.target.value,
+    });
+  };
+
+  return (
+    <Center minH="100vh" py="10">
+      <Container maxW="md">
+        <BrandFavicon mx="auto" mb="4" />
+        <Heading mb="4" textAlign="center">
+          Forgot your password?
+        </Heading>
+        <Text mb="10" fontWeight="medium" color="gray.700" textAlign="center">
+          No worries, we&apos;ll send you reset instructions.
+        </Text>
+        <Box
+          as="form"
+          w="full"
+          onSubmit={(e: any) => {
+            e.preventDefault();
+          }}
+        >
+          <FormControl isInvalid={state.error !== ""} mb="5">
+            <Input
+              type="email"
+              name="email"
+              bg="white"
+              placeholder="Enter your email"
+              value={state.value}
+              onChange={onChange}
+            />
+            <FormErrorMessage>{state.error}</FormErrorMessage>
+          </FormControl>
+          <Button type="submit" mt="8" size="md" colorScheme="blue" w="full">
+            Reset password
+          </Button>
+        </Box>
+        <Box textAlign="center" mt="4">
+          <NextLink href="/auth/signup" passHref>
+            <Link color="gray.600" fontWeight="500" justifyItems="center">
+              <ArrowBackIcon mr="1" />
+              Back to login
+            </Link>
+          </NextLink>
+        </Box>
+      </Container>
+    </Center>
+  );
+}
+
+export default Recover;
