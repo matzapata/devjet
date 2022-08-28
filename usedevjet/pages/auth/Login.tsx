0cfbbb92f19a880037376aa0c5154986ab838391
Matias Zapata <matuzapata@gmail.com>
2022-08-28 17:27:08 -0300

    Login component styles

--- usedevjet/pages/auth/Login.tsx
+++ usedevjet/pages/auth/Login.tsx
@@ -9,6 +9,10 @@ import {
   Input,
   VStack,
   Link,
+  Center,
+  Box,
+  Text,
+  Flex,
 } from "@chakra-ui/react";
 import { BrandFavicon } from "../../components/Brand";
 import NextLink from "next/link";
@@ -34,21 +38,25 @@ export default function Login() {
   };
 
   return (
-    <Container>
-      <VStack>
-        <BrandFavicon />
-        <Heading>Sign in to your account</Heading>
-        <VStack
+    <Center minH="100vh">
+      <Container maxW="md">
+        <BrandFavicon mx="auto" mb="4" />
+        <Heading mb="20" textAlign="center">
+          Log in to your account
+        </Heading>
+        <Box
           as="form"
-          onSubmit={(e) => {
+          w="full"
+          onSubmit={(e: any) => {
             e.preventDefault();
           }}
         >
-          <FormControl isInvalid={state.email.error !== ""}>
+          <FormControl isInvalid={state.email.error !== ""} mb="5">
             <FormLabel>Email</FormLabel>
             <Input
               type="email"
               name="email"
+              bg="white"
               value={state.email.value}
               onChange={onChange}
             />
@@ -59,17 +67,38 @@ export default function Login() {
             <Input
               type="password"
               name="password"
+              bg="white"
               value={state.password.value}
               onChange={onChange}
             />
             <FormErrorMessage>{state.password.error}</FormErrorMessage>
           </FormControl>
-          <Link as={NextLink} href="/auth/recover" passHref>
-            Forgot your password?
-          </Link>
-          <Button colorScheme="blue">Sign In</Button>
-        </VStack>
-      </VStack>
-    </Container>
+          <NextLink href="/auth/recover" passHref>
+            <Link
+              color="blue.600"
+              fontWeight="medium"
+              display="block"
+              textAlign="right"
+              mt="1"
+            >
+              Forgot your password?
+            </Link>
+          </NextLink>
+          <Button mt="8" size="md" colorScheme="blue" w="full">
+            Sign In
+          </Button>
+        </Box>
+        <Flex justifyContent="center" mt="6">
+          <Text as="span" fontWeight="500" mr="1" color="gray.600">
+            Don&apos;t have an account?
+          </Text>
+          <NextLink href="/auth/signup" passHref>
+            <Link color="blue.600" fontWeight="500">
+              Sign up
+            </Link>
+          </NextLink>
+        </Flex>
+      </Container>
+    </Center>
   );
 }
