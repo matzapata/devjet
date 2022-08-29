5837ebef04191373dda45239f20080b7bef60867
Matias Zapata <matuzapata@gmail.com>
2022-08-29 20:16:26 -0300

    Auth0 working properly. Missing corresponding UI

--- usedevjet/pages/_app.tsx
+++ usedevjet/pages/_app.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import theme from "../utils/chakraTheme";
 import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
   );
 };
 
