a55cdae5169f94aea240b12bb8ffe7443302383c
Matias Zapata <matuzapata@gmail.com>
2022-09-05 22:10:51 -0300

    CLI publish, eslint and remove existing path

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -6,11 +6,15 @@ import { build } from 'gluegun'
 async function run(argv) {
   // create a CLI runtime
   const cli = build()
-    .brand('cli')
+    .brand('devjet')
     .src(__dirname)
     .plugins('./node_modules', { matching: 'cli-*', hidden: true })
     .help() // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
+    .defaultCommand({
+      name: 'default',
+      run: (toolbox) => toolbox.print.printHelp(toolbox),
+    })
     .create()
   // enable the following method if you'd like to skip loading one of these core extensions
   // this can improve performance if they're not necessary for your project:
