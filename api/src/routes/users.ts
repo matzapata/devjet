db3254530e1ca6d03d5eed3e43e9143e3629ab6b
Matias Zapata <matuzapata@gmail.com>
2022-09-11 15:23:33 -0300

    Remove express api and intitialize prisma

--- api/src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-const router: express.Router = express.Router();
-
-router.get("/", (req: express.Request, res: express.Response) => {
-  res.send("User index");
-});
-
-export default router;
