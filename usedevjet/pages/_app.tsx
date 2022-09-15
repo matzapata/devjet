203a045984e6ac0e3a6e3a35644c1c5439bb16ab
Matias Zapata <matuzapata@gmail.com>
2022-09-15 16:17:54 -0300

    Configure redux

--- usedevjet/pages/_app.tsx
+++ usedevjet/pages/_app.tsx
@@ -5,14 +5,18 @@ import theme from "../utils/chakraTheme";
 import "../styles/global.css";
 import { UserProvider } from "@supabase/auth-helpers-react";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Provider } from "react-redux";
+import { store } from "redux/store";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <UserProvider supabaseClient={supabaseClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider supabaseClient={supabaseClient}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </UserProvider>
+    </Provider>
   );
 };
 
