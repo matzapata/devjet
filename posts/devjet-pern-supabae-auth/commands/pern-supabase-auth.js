8e05fa15dfd4f46846d1b368443a5b103b22b33c
Matias Zapata <matuzapata@gmail.com>
2022-09-20 11:13:59 -0300

    devjet new post commit

---
+++ posts/devjet-pern-supabae-auth/commands/pern-supabase-auth.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    name: 'pern-supabase-auth',
+    description: 'Add authentication with supabase to your pern project',
+    run: async (toolbox) => {
+        toolbox.print.info("Add authentication with supabase to your pern project")
+    }
+}
