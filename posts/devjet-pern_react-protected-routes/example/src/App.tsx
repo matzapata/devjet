7a6d30160cdcb4e5f0d279651a102ad191db3ed7
Matias Zapata <matuzapata@gmail.com>
2022-09-26 17:26:50 -0300

    Create post and publish

--- posts/devjet-pern_react-protected-routes/example/src/App.tsx
+++ posts/devjet-pern_react-protected-routes/example/src/App.tsx
@@ -3,11 +3,24 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
+import Protected from "pages/Protected";
+
+import ProtectedRoute from "components/ProtectedRoute";
 
 function App() {
+  const user = true;
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route
+        element={
+          <ProtectedRoute isAllowed={!!user} redirectPath="/auth/login" />
+        }
+      >
+        <Route path="protected" element={<Protected />} />
+        <Route path="test" element={<Protected />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
