a8793d648fd6da8971922681b0923c442e671827
Matias Zapata <matuzapata@gmail.com>
2022-09-26 14:46:38 -0300

    CLI improved help command and generator utilities

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,4 +1,5 @@
-import { build } from 'gluegun'
+import { build } from 'gluegun';
+import helpCommand from './commands/help';
 
 /**
  * Create the cli and kick it off
@@ -9,21 +10,17 @@ async function run(argv) {
     .brand('devjet')
     .src(__dirname)
     .plugins('./node_modules', { matching: 'devjet-*', hidden: true })
-    .help() // provides default for help, h, --help, -h
     .version() // provides default for version, v, --version, -v
-    .defaultCommand({
-      name: 'default',
-      run: (toolbox) => toolbox.print.printHelp(toolbox),
-    })
-    .create()
+    .defaultCommand(helpCommand)
+    .create();
   // enable the following method if you'd like to skip loading one of these core extensions
   // this can improve performance if they're not necessary for your project:
   // .exclude(['meta', 'strings', 'print', 'filesystem', 'semver', 'system', 'prompt', 'http', 'template', 'patching', 'package-manager'])
   // and run it
-  const toolbox = await cli.run(argv)
+  const toolbox = await cli.run(argv);
 
   // send it back (for testing, mostly)
-  return toolbox
+  return toolbox;
 }
 
-module.exports = { run }
+module.exports = { run };
---
+++ cli/src/commands/help.ts
@@ -0,0 +1,35 @@
+import { GluegunToolbox } from 'gluegun';
+
+const command = {
+  name: 'help',
+  alias: 'h',
+  run: (toolbox: GluegunToolbox): void => {
+    console.log(
+      toolbox.print.colors.bold('\nUsage: devjet <command> [OPTIONS]')
+    );
+    console.log(toolbox.print.colors.bold('\nCommands:'));
+    toolbox.runtime.plugins.forEach((plugin) => {
+      if (plugin.name !== 'devjet')
+        console.log(`${toolbox.print.colors.bold(plugin.name)} - (plugin)`);
+
+      plugin.commands.forEach((command) => {
+        let help = `  ${command.name}`;
+        if (command.description) help += ` - ${command.description}`;
+        toolbox.print.info(help);
+      });
+    });
+
+    console.log(toolbox.print.colors.bold('\nFurther help:'));
+    toolbox.print.info(
+      '  To add plugins check out receipes at www.usedevjet.com'
+    );
+    toolbox.print.info(
+      '  Except for the stack generators, we recomend you to remove plugins once used'
+    );
+    toolbox.print.info('  Remove plugins with "npm uninstall plugin_name"');
+
+    console.log('\n');
+  },
+};
+
+export default command;
