ce6755177e02b0fc732a57aa56c79ca080f684ed
Matias Zapata <matuzapata@gmail.com>
2022-09-27 11:00:03 -0300

    Newsletter generator

--- posts/devjet-newsletter-with-revue/src/commands/index.ts
+++ posts/devjet-newsletter-with-revue/src/commands/index.ts
@@ -3,16 +3,29 @@ import getContext from '../lib/context';
 import step from '../lib/step';
 
 module.exports = {
-  name: '',
-  description: '',
+  name: 'newsletter-with-revue',
+  description: 'Create a newsletter component with revue',
   run: async (toolbox: GluegunToolbox) => {
     const { stack } = await getContext(toolbox);
 
-    await step(toolbox, '1. First step', stack, {
-      all: async () => console.log('All actions'),
-      react: async () => console.log('React actions'),
-      pern: async () => console.log('Pern actions'),
-      nextjs: async () => console.log('Nextjs actions'),
+    await step(toolbox, '1. Create your account at https://www.getrevue.co/', stack, {
+      all: async () => console.log('This one is on you 😉​'),
+    });
+
+    await step(toolbox, '2. Create the newsletter component', stack, {
+      all: async () => console.log('Remember to update the username in your form action url'),
+      react: () => toolbox.template.generate({
+        template: "Newsletter.tsx",
+        target: "src/components/Newsletter.tsx"
+      }),
+      pern: () => toolbox.template.generate({
+        template: "Newsletter.tsx",
+        target: "client/src/components/Newsletter.tsx"
+      }),
+      nextjs: () => toolbox.template.generate({
+        template: "Newsletter.tsx",
+        target: "components/Newsletter.tsx"
+      }),
     });
 
     toolbox.print.success('All done!!');
