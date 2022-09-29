cdf94c38e0109c13706de36c65c996996bd09e0a
Matias Zapata <matuzapata@gmail.com>
2022-09-29 17:26:14 -0300

    Removed

--- usedevjet/pages/_app.tsx
+++ usedevjet/pages/_app.tsx
@@ -5,19 +5,15 @@ import theme from "../lib/chakraTheme";
 import "../styles/global.css";
 import { UserProvider } from "@supabase/auth-helpers-react";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
-import { Provider } from "react-redux";
-import { store } from "redux/store";
 import NextNProgress from "nextjs-progressbar";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider supabaseClient={supabaseClient}>
-      <Provider store={store}>
-        <ChakraProvider theme={theme}>
-          <NextNProgress />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </Provider>
+      <ChakraProvider theme={theme}>
+        <NextNProgress />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </UserProvider>
   );
 };
