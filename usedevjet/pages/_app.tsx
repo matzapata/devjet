62f99fa72c13a4751f0934f8d0ac10d471ec14a9
Matias Zapata <matuzapata@gmail.com>
2022-09-28 21:11:15 -0300

    Add NextNProgress

--- usedevjet/pages/_app.tsx
+++ usedevjet/pages/_app.tsx
@@ -7,12 +7,14 @@ import { UserProvider } from "@supabase/auth-helpers-react";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Provider } from "react-redux";
 import { store } from "redux/store";
+import NextNProgress from "nextjs-progressbar";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider supabaseClient={supabaseClient}>
       <Provider store={store}>
         <ChakraProvider theme={theme}>
+          <NextNProgress />
           <Component {...pageProps} />
         </ChakraProvider>
       </Provider>
