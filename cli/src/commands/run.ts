8cc99d9520dfa483ebde3c6a743b8066576d3718
Matias Zapata <matuzapata@gmail.com>
2022-10-02 20:46:24 -0300

    Build plugin runner

--- cli/src/commands/run.ts
+++ cli/src/commands/run.ts
@@ -1,4 +1,4 @@
-import { GluegunToolbox } from 'gluegun';
+import { filesystem, GluegunToolbox } from 'gluegun';
 import findGitRoot from 'find-git-root';
 
 function getProjectStack(toolbox: GluegunToolbox, root: string) {
@@ -20,30 +20,112 @@ function findProjectRoot() {
   return findGitRoot(process.cwd()).replace('/.git', '').replace('\\.git', '');
 }
 
-// function printCommands(toolbox: GluegunToolbox, commands: GluegunCommand[]) {
-//   commands.forEach((command) =>
-//     toolbox.print.info(`  ${command.name} - ${command.description}`)
-//   );
-// }
+async function runPlugin(
+  toolbox: GluegunToolbox,
+  context: {
+    stack: string;
+    root: string;
+  },
+  pluginFolder: string,
+  command: string
+) {
+  const pluginToolbox = {
+    context,
+    parameters: toolbox.parameters,
+    filesystem: toolbox.filesystem,
+    http: toolbox.http,
+    meta: toolbox.meta,
+    patching: toolbox.patching,
+    print: toolbox.print,
+    prompt: toolbox.prompt,
+    semver: toolbox.semver,
+    strings: toolbox.strings,
+    system: toolbox.system,
+    template: {
+      generate: ({
+        template,
+        target,
+        props,
+        directory,
+      }: {
+        template: string;
+        target: string;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        props?: any;
+        directory?: string;
+      }) => {
+        toolbox.template.generate({
+          directory: directory
+            ? directory
+            : toolbox.filesystem.path(pluginFolder, 'build', 'templates'),
+          template,
+          target,
+          props,
+        });
+      },
+    },
+    packageManager: toolbox.packageManager,
+    step: async (
+      message: string,
+      action?: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        all?: () => Promise<any>;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        react?: () => Promise<any>;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        pern?: () => Promise<any>;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        nextjs?: () => Promise<any>;
+      }
+    ): Promise<void> => {
+      try {
+        if (action === undefined) {
+          return toolbox.print.success(`${toolbox.print.checkmark} ${message}`);
+        }
 
-// async function runPlugin(toolbox, pluginName, commandName) {
-//   console.log(pluginName, commandName);
-//   const runner = build().plugin(`./node_modules/${pluginName}`).create();
-//   console.log(runner);
-//   const res = await runner.run(commandName);
-//   console.log(res);
-//   return null;
-// }
+        if (action.all !== undefined) await action.all();
+        if (action[context.stack] !== undefined) await action[context.stack]();
+        toolbox.print.success(`${toolbox.print.checkmark} ${message}`);
+      } catch (e) {
+        toolbox.print.error(`${toolbox.print.xmark}  ${message}`);
+        toolbox.print.error(`Error message: ${e.message}`);
+      }
+    },
+  };
+
+  const isReady = await toolbox.prompt.confirm(
+    'We strongly recommend to run devjet generators on a new branch or with a previous commit. Are you ready to continue?'
+  );
+  if (!isReady) return toolbox.print.error('Take your time, no problem!');
+
+  toolbox.print.warning(
+    'Hey, if you changed the default folder structure, some things may break...\n'
+  );
+
+  const commandPath = toolbox.filesystem.path(
+    pluginFolder,
+    'build',
+    'commands',
+    `${command}.js`
+  );
+
+  const plugin = await import(commandPath);
+  if (plugin) await plugin.run(pluginToolbox);
+  else toolbox.print.error('Auchh, it seams like that command does not exist');
+
+  return null;
+}
 
 module.exports = {
   name: 'run',
   description: `Bootstrap your Devjet project. Usage: devjet new projectname`,
   run: async (toolbox: GluegunToolbox) => {
-    const { print, parameters } = toolbox;
+    const { print, parameters, prompt } = toolbox;
     const helpMessage = 'Usage: devjet run pluginname commandname';
 
     // npx devjet run pluginname commandname
-    const pluginName = parameters.first;
+    const dev = parameters.options.dev;
+    const pluginName = `devjet-${parameters.first}`;
     const commandName = parameters.second;
     if (!pluginName) return toolbox.print.error(helpMessage);
     if (!commandName) return toolbox.print.error(helpMessage);
@@ -58,34 +140,43 @@ module.exports = {
     else print.info(`Great! it seams you are working with ${stack}`);
 
     // Install pluginname
-    const pluginInstallSpinner = toolbox.print.spin(
-      'Installing plugin package'
-    );
-    pluginInstallSpinner.start();
-    const pluginPackage = await toolbox.packageManager.add(pluginName, {
-      dev: true,
-    });
-    if (!pluginPackage.success) {
-      return pluginInstallSpinner.fail("Couldn't install plugin");
+    if (dev) {
+      print.warning(
+        '\nYou are in dev mode, meaning you will have to install and uninstall the plugin package yourself. Use npm install ../path/to/package or npm link\n'
+      );
+    } else {
+      const pluginInstallSpinner = toolbox.print.spin(
+        'Installing plugin package'
+      );
+      pluginInstallSpinner.start();
+      const pluginPackage = await toolbox.packageManager.add(pluginName, {
+        dev: true,
+      });
+      if (!pluginPackage.success) {
+        return pluginInstallSpinner.fail("Couldn't install plugin");
+      }
+      pluginInstallSpinner.succeed('Plugin installed');
     }
-    pluginInstallSpinner.succeed('Plugin installed');
 
-    // await runPlugin(toolbox, pluginName, commandName);
-    toolbox.runtime.addPlugin(`./node_modules/${pluginName}`);
-    await toolbox.runtime.run(commandName);
+    const pluginPath = filesystem.path('.', 'node_modules', pluginName);
+    await runPlugin(toolbox, { stack, root }, pluginPath, commandName);
 
     // Prompt for uninstall
-    // const removePlugin = await prompt.confirm(
-    //   'Would you like to remove the plugin (recommended if one time)',
-    //   true
-    // );
-    // if (removePlugin) {
-    //   const pluginRemoveSpinner = toolbox.print.spin('Removing plugin package');
-    //   pluginRemoveSpinner.start();
-    //   await toolbox.packageManager.remove(pluginName, { dryRun: false });
-    //   pluginRemoveSpinner.succeed('Plugin removed successfully');
-    // }
+    if (!dev) {
+      const removePlugin = await prompt.confirm(
+        'Would you like to remove the plugin (recommended if one time)',
+        true
+      );
+      if (removePlugin) {
+        const pluginRemoveSpinner = toolbox.print.spin(
+          'Removing plugin package'
+        );
+        pluginRemoveSpinner.start();
+        await toolbox.packageManager.remove(pluginName, { dryRun: false });
+        pluginRemoveSpinner.succeed('Plugin removed successfully');
+      }
+    }
 
-    // return print.success(`${print.checkmark} All done!`);
+    return print.success(`${print.checkmark} All done!`);
   },
 };
--- cli/src/extensions/cli-extension.ts
+++ cli/src/extensions/cli-extension.ts
@@ -3,8 +3,26 @@ import { GluegunToolbox } from 'gluegun';
 // add your CLI-specific functionality here, which will then be accessible
 // to your commands
 module.exports = (toolbox: GluegunToolbox) => {
-  toolbox.foo = () => {
-    toolbox.print.info('called foo extension');
+  toolbox.step = async (
+    toolbox: GluegunToolbox,
+    message: string,
+    stack: string,
+    action: {
+      all?: () => Promise<any>;
+      react?: () => Promise<any>;
+      pern?: () => Promise<any>;
+      nextjs?: () => Promise<any>;
+    }
+  ): Promise<void> => {
+    try {
+      if (action.all !== undefined) await action.all();
+      if (action[stack] !== undefined) await action[stack]();
+
+      toolbox.print.success(`${toolbox.print.checkmark} ${message}`);
+    } catch (e) {
+      toolbox.print.error(`${toolbox.print.xmark}  ${message}`);
+      toolbox.print.error(`Error message: ${e.message}`);
+    }
   };
 
   // enable this if you want to read configuration in from
