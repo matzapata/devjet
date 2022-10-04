b5dd0bb9d538c0fc4c646aa6f4c954480fed5d7f
Matias Zapata <matuzapata@gmail.com>
2022-10-03 23:18:11 -0300

    Remove step from extension

--- cli/src/extensions/cli-extension.ts
+++ cli/src/extensions/cli-extension.ts
@@ -2,29 +2,9 @@ import { GluegunToolbox } from 'gluegun';
 
 // add your CLI-specific functionality here, which will then be accessible
 // to your commands
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = (toolbox: GluegunToolbox) => {
-  toolbox.step = async (
-    toolbox: GluegunToolbox,
-    message: string,
-    stack: string,
-    action: {
-      all?: () => Promise<any>;
-      react?: () => Promise<any>;
-      pern?: () => Promise<any>;
-      nextjs?: () => Promise<any>;
-    }
-  ): Promise<void> => {
-    try {
-      if (action.all !== undefined) await action.all();
-      if (action[stack] !== undefined) await action[stack]();
-
-      toolbox.print.success(`${toolbox.print.checkmark} ${message}`);
-    } catch (e) {
-      toolbox.print.error(`${toolbox.print.xmark}  ${message}`);
-      toolbox.print.error(`Error message: ${e.message}`);
-    }
-  };
-
   // enable this if you want to read configuration in from
   // the current folder's package.json (in a "cli" property),
   // cli.config.json, etc.
