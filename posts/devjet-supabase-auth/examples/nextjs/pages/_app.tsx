4de8432492d36c6b47cef698986c09d922604b33
Matias Zapata <matuzapata@gmail.com>
2022-10-08 21:44:50 -0300

    Publish supabase-auth posts

--- posts/devjet-supabase-auth/examples/nextjs/pages/_app.tsx
+++ posts/devjet-supabase-auth/examples/nextjs/pages/_app.tsx
@@ -1,14 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import chakraTheme from "../utils/chakraTheme"
+import React from "react";
+import "styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import chakraTheme from "utils/chakraTheme";
+import { UserProvider } from "@supabase/auth-helpers-react";
+import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={chakraTheme}>
-      <Component {...pageProps} />
+      <UserProvider supabaseClient={supabaseClient}>
+        <Component {...pageProps} />
+      </UserProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
