8053f3f991a5a5aded74a099b0a6ec5982e8034a
Matias Zapata <matuzapata@gmail.com>
2022-10-13 23:26:53 -0300

    usedevjet.com cloudflare analytics

--- usedevjet/pages/_document.tsx
+++ usedevjet/pages/_document.tsx
@@ -11,6 +11,11 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={"dark"} />
           <Main />
           <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "3e49eff50a034dabb54c4a965a2d7e93"}'
+          ></script>
         </body>
       </Html>
     );
